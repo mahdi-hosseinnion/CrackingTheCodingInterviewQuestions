d9034e979a1c640d6546c110f36a67be77d929c2
mahdi <ssmmhh1382@gmail.com>
2022-05-30 12:03:56 +0430

    use generic data type in Node instead of int + clean Main.java

--- src/com/company/Node.java
+++ src/com/company/Node.java
@@ -1,26 +1,46 @@
 package com.company;
 
-class Node {
+class Node<DATA> {
 
-    public Node next = null;
+    public Node<DATA> next = null;
 
-    int data;
+    DATA data;
 
-    public Node(int data) {
+    public Node(DATA data) {
         this.data = data;
     }
 
-    void append(int data) {
-        Node end = new Node(data);
-        Node n = this;
-
+    void append(DATA data) {
+        Node<DATA> end = new Node<>(data);
+        Node<DATA> n = this;
         while (n.next != null) {
             n = n.next;
         }
         n.next = end;
     }
 
-    void remove() {
-
+    public Node<DATA> copy() {
+        Node<DATA> result = new Node<>(this.data);
+        Node<DATA> node = this.next;
+        while (node != null) {
+            result.append(node.data);
+            node = node.next;
+        }
+        return result;
     }
+
+    public void printAll(String separator) {
+        Node<DATA> currentNode = this;
+        while (currentNode != null) {
+            DATA item = currentNode.data;
+            System.out.print(item + separator);
+            currentNode = currentNode.next;
+        }
+        System.out.println();
+    }
+
+    public void printAll() {
+        this.printAll(" ");
+    }
+
 }
