fbb8aa3c97d0038f09cac2b7db1da4e2b35c7d01
mahdi <ssmmhh1382@gmail.com>
2022-06-08 16:57:36 +0430

    create hasRouteBetweenNodesWithBFS function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,10 @@
 package com.company;
 
 
+import com.company.graph.Graph;
+import com.company.util.Queue;
+
+import java.util.HashSet;
 import java.util.Scanner;
 
 
@@ -10,6 +14,31 @@ public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    private static <T> boolean hasRouteBetweenNodesWithBFS(Graph.Node<T> source, Graph.Node<T> destination) {
+        Queue<Graph.Node<T>> nextToSearch = new Queue<>();
+        HashSet<Long> visitedNodes = new HashSet<>();
+
+        nextToSearch.enqueue(source);
+
+        while (!nextToSearch.isEmpty()) {
+            Graph.Node<T> item = nextToSearch.dequeue();
+
+            if (item == destination) {
+                return true;
+            }
+
+            if (visitedNodes.contains(item.getId())) {
+                continue;
+            }
+            visitedNodes.add(item.getId());
+
+            for (Graph.Node<T> child : item.adjacent) {
+                nextToSearch.enqueue(child);
+            }
+        }
+        return false;
     }
 
 }
