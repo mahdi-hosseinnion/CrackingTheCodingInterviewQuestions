394b4eb3eb8fa848ceeb8830ac388f024c17015f
mahdi <ssmmhh1382@gmail.com>
2022-07-07 16:19:28 +0430

    complete the brute force solution

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,26 +12,20 @@ public class Main {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        CallCenter callCenter = new CallCenter();
-        while (true) {
-            System.out.println("Insert call information");
-            System.out.println("What is the caller name?");
-            String callerName = sc.nextLine();
-            System.out.println("What is the call duration?");
-            int callDuration = sc.nextInt();
-            sc.nextLine();
-            System.out.println("What is your desire call handler? r,m,d");
-            char handler = sc.nextLine().charAt(0);
-            Call.CallHandler callHandler = Call.CallHandler.RESPONDENT;
-            if (handler == 'm') {
-                callHandler = Call.CallHandler.MANAGER;
-            } else if (handler == 'd') {
-                callHandler = Call.CallHandler.DIRECTOR;
-            }
-            Call call = new Call(callerName, callDuration, callHandler);
-            callCenter.dispatchCall(call);
-        }
+        int n = sc.nextInt();
+        System.out.println("");
+        System.out.println("There is #" + steps(n) + " ways.");
     }
 
+    public static int steps(int n) {
+        if (n < 0) return 0;
+        if (n == 0) return 1;
+        if (n == 1) return 1;
 
+        int oneSum = steps(n - 1);
+        int twoSum = steps(n - 2);
+        int treeSum = steps(n - 3);
+
+        return oneSum + twoSum + treeSum;
+    }
 }
